5fbe4c5128a4c5f2dbe5f08b3b13aed0e3457825
xla27 <alberto.perlini99@gmail.com>
2025-01-20 12:55:12 +0100

    Debug

--- AIR5/__init__.py
+++ AIR5/__init__.py
@@ -2,4 +2,4 @@ from mlmc import mlmc, screening
 from mlmc_plot import mlmc_plot
 from level_dw5 import dw_l
 from anova import anova
-from anova import anova_plot
+from anova_plot import anova_plot
