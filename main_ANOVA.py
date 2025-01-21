56ed92ace132d8b53073c3af105407cacb2611f2
xla27 <alberto.perlini99@gmail.com>
2025-01-21 10:11:28 +0100

    Limiting numpy threads

--- main_ANOVA.py
+++ main_ANOVA.py
@@ -1,4 +1,8 @@
 import os, sys
+os.environ["MKL_NUM_THREADS"] = "1" 
+os.environ["NUMEXPR_NUM_THREADS"] = "1" 
+os.environ["OMP_NUM_THREADS"] = "1" 
+os.environ['OPENBLAS_NUM_THREADS'] = "1"
 
 def main(nproc, workingFolder):
 
