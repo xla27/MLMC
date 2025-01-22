251d9bf16be47024be505506304ba755dbbf18d1
xla27 <alberto.perlini99@gmail.com>
2025-01-22 14:51:19 +0100

    Relative imports correction

--- AIR11_AMG/__init__.py
+++ AIR11_AMG/__init__.py
@@ -1,5 +1,4 @@
-from AIR11.mlmc import mlmc, screening
-from AIR11.mlmc_plot import mlmc_plot
-from level_dw11 import dw_l
-from anova import anova
-from AIR11.anova_plot import anova_plot
+from .cfd_call   import cfd_call as cfd_call_amg
+from .level_dw11 import dw_l
+from .sobol      import compute_sobol_indices
+from .anova      import anova
--- AIR11_AMG/anova.py
+++ AIR11_AMG/anova.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.interpolate import interp1d
 from SALib.sample import saltelli
 
-from AIR11_AMG.sobol import compute_sobol_indices
+from AIR11_AMG import compute_sobol_indices
 
 def anova(problem, lev, sample_sizes, *varargin):
 
