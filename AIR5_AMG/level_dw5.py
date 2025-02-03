bf3dc51fa6bfb0c989c7242356f0c4a1f1e502b0
xla27 <alberto.perlini99@gmail.com>
2025-02-03 14:40:51 +0100

    Update The level MC is completely changed due to the fact that when using mesh adaptation algorithm it is not guaranteed that the number of wall points is constant for a given level. Therefore, the new approach is based on computing all the samples, and then interpolating on the x of the coarse with the lowest number of points. Then, the other coarse are interpolated on the "coarsest coarse". Lastly, the fines are interpolated on the coarsest. Then, statistics are computed.

--- AIR5_AMG/level_dw5.py
+++ AIR5_AMG/level_dw5.py
@@ -5,7 +5,7 @@ import time
 from AIR5_AMG import cfd_call_amg
 from AIR5     import cfd_call
 
-def dw_l(l, N_samples, *args):
+def dw_l(level, N_samples, *args):
     """
     Function handling the CFD simulations and the difference between fine and coarse at given level.
 
@@ -61,21 +61,24 @@ def dw_l(l, N_samples, *args):
     Pmean   = 390;   P_max   = 600;   P_min   = 300
     Bn2mean = 0.79;  Bn2_max = 0.8;   Bn2_min = 0.76
 
+    QoI_coarse = [None] * N_samples
+    QoI_fine   = [None] * N_samples
+
     # Looping over the samples
-    for i in range(1, N_samples  + 1):
+    for i in range(N_samples):
         
         # Sampling the aleatoric uncertainties
-        M_inf = M_min + (M_max - M_min) * np.random.rand()
+        M_inf    = M_min + (M_max - M_min) * np.random.rand()
         T_inf    = T_min + (T_max - T_min) * np.random.rand()
         P_inf    = P_min + (P_max - P_min) * np.random.rand() 
         Bn2_inf  = Bn2_min + (Bn2_max - Bn2_min) * np.random.rand() 
         Bo2_inf  = 1 - Bn2_inf
         
         # Formatting for printing
-        M_inf = '{:.3f}'.format(M_inf)
-        T_inf    = '{:.1f}'.format(T_inf)
-        P_inf    = '{:.1f}'.format(P_inf)
-        Bn2_inf  = '{:.3f}'.format(Bn2_inf)
+        M_inf   = '{:.3f}'.format(M_inf)
+        T_inf   = '{:.1f}'.format(T_inf)
+        P_inf   = '{:.1f}'.format(P_inf)
+        Bn2_inf = '{:.3f}'.format(Bn2_inf)
         Bo2_inf = '{:.3f}'.format(Bo2_inf)
         
         # Strings to be inserted in SU2 configs
@@ -87,129 +90,109 @@ def dw_l(l, N_samples, *args):
 
         SF = 0.0075 
 
-        if l == 0:
-            # No call to CFD with coearse mesh, coarse results set to zero as it is the starting level
-            Pc       = 0 
-            beta_Nc  = 0
-            beta_Oc  = 0 
-            beta_NOc = 0
-            beta_N2c = 0
-            beta_O2c = 0
-            Ttr_ic   = 0
-            Tve_ic   = 0
-            M_ic     = 0
+        # return of CFD calls [beta_n, beta_o, beta_no, beta_n2, beta_o2, P, Ttr, Tve, M, xnodes]
+
+        if level == 0:
 
             # Call to CFD with fine mesh (no adaptation)
             (_, baseFolder, workingFolder) = args
             baseFolder2 = baseFolder.replace('AIR5_AMG','AIR5')
             args2 = (nproc, baseFolder2, workingFolder)
-            beta_Nf, beta_Of, beta_NOf, beta_N2f, beta_O2f, P_if, Ttr_if, Tve_if, M_if, xnodesf = cfd_call('FINE', valIns_M, valIns_T, valIns_P, valIns_Bn2, valIns_Bo2, l, i, *args2)
+            QoI_fine[i] = list(cfd_call('FINE', valIns_M, valIns_T, valIns_P, valIns_Bn2, valIns_Bo2, level, i, *args2))
 
-            
-            xnodesc = xnodesf    # No need for interpolation on coarse, xnodes put equal
-
-            Pf = P_if
-            ws = max(int(len(Pf) * SF), 1)    # window size
-
-            # Moving average on wall quantities
-            Pf       = moving_average(Pf,       ws)
-            beta_Nf  = moving_average(beta_Nf,  ws)
-            beta_Of  = moving_average(beta_Of,  ws)
-            beta_NOf = moving_average(beta_NOf, ws)
-            beta_N2f = moving_average(beta_N2f, ws)
-            beta_O2f = moving_average(beta_O2f, ws)
-            Ttr_if   = moving_average(Ttr_if,   ws)
-            Tve_if   = moving_average(Tve_if,   ws)
-            M_if     = moving_average(M_if,     ws)   
+            # No call to CFD with coearse mesh, coarse results set to zero as it is the starting level
+            xnodesc = QoI_fine[i][-1]
+            QoI_coarse[i] = [[0.] * len(xnodesc) for i in range(0,9)]
+            QoI_coarse[i].append(xnodesc)
 
         else:
-            # Call to CFD with fine mesh
-            beta_Nf, beta_Of, beta_NOf, beta_N2f, beta_O2f, P_if, Ttr_if, Tve_if, M_if, xnodesf = cfd_call_amg('FINE',valIns_M, valIns_T, valIns_P, valIns_Bn2, valIns_Bo2, l, i, *args)
 
-            Pf = P_if
+            # Call to CFD with fine mesh
+            QoI_fine[i] = list(cfd_call_amg('FINE',valIns_M, valIns_T, valIns_P, valIns_Bn2, valIns_Bo2, level, i, *args))
 
             # Call to CFD with coarse mesh
-            beta_Nc, beta_Oc, beta_NOc, beta_N2c, beta_O2c, p_ic, Ttr_ic, Tve_ic, M_ic, xnodesc = cfd_call_amg('COARSE', valIns_M, valIns_T, valIns_P, valIns_Bn2, valIns_Bo2, l, i, *args)
+            QoI_coarse[i] = list(cfd_call_amg('COARSE', valIns_M, valIns_T, valIns_P, valIns_Bn2, valIns_Bo2, level, i, *args))
 
-            Pc = p_ic
-            ws = max(int(len(Pc) * SF), 1) 
 
-            # Results interpolation on the coarser grid            
-            Pf       = interp1d(xnodesf, Pf,       kind='linear', fill_value='extrapolate')(xnodesc) 
-            beta_Nf  = interp1d(xnodesf, beta_Nf,  kind='linear', fill_value='extrapolate')(xnodesc)
-            beta_Of  = interp1d(xnodesf, beta_Of,  kind='linear', fill_value='extrapolate')(xnodesc)
-            beta_NOf = interp1d(xnodesf, beta_NOf, kind='linear', fill_value='extrapolate')(xnodesc)                   
-            beta_N2f = interp1d(xnodesf, beta_N2f, kind='linear', fill_value='extrapolate')(xnodesc)
-            beta_O2f = interp1d(xnodesf, beta_O2f, kind='linear', fill_value='extrapolate')(xnodesc) 
-            Ttr_if   = interp1d(xnodesf, Ttr_if,   kind='linear', fill_value='extrapolate')(xnodesc) 
-            Tve_if   = interp1d(xnodesf, Tve_if,   kind='linear', fill_value='extrapolate')(xnodesc) 
-            M_if     = interp1d(xnodesf, M_if,     kind='linear', fill_value='extrapolate')(xnodesc) 
+    # Extracting the reference xnodes for the coarse level as the xnodes component of COARSE samples with the lowest number of elements
+    xnodesc_list = [QoI_coarse[i][-1] for i in range(N_samples)]
 
-            # Moving average on fine wall quantities
-            Pf       = moving_average(Pf,       ws)
-            beta_Nf  = moving_average(beta_Nf,  ws)
-            beta_Of  = moving_average(beta_Of,  ws)
-            beta_NOf = moving_average(beta_NOf, ws)
-            beta_N2f = moving_average(beta_N2f, ws)
-            beta_O2f = moving_average(beta_O2f, ws)
-            Ttr_if   = moving_average(Ttr_if,   ws)
-            Tve_if   = moving_average(Tve_if,   ws)
-            M_if     = moving_average(M_if,     ws) 
+    xnodesc_ref = min(xnodesc_list, key=len)
 
-            # Moving average on coearse wall quantities
-            Pc       = moving_average(Pc,       ws)
-            beta_Nc  = moving_average(beta_Nc,  ws)
-            beta_Oc  = moving_average(beta_Oc,  ws)
-            beta_NOc = moving_average(beta_NOc, ws)
-            beta_N2c = moving_average(beta_N2c, ws)
-            beta_O2c = moving_average(beta_O2c, ws)
-            Ttr_ic   = moving_average(Ttr_ic,   ws)
-            Tve_ic   = moving_average(Tve_ic,   ws)
-            M_ic     = moving_average(M_ic,     ws)
+    # windows size for moving average
+    ws = max(int(len(xnodesc_ref) * SF), 1)
+
+    QoI_coarse_interp = np.zeros((N_samples, 9, len(xnodesc_ref)))
+    QoI_fine_interp   = np.zeros((N_samples, 9, len(xnodesc_ref)))
+
+    # Interpolating the coarse and fine on the reference coarse and doing the moving average of the samples
+    for i in range(N_samples):
+
+        for j in range(0,9):
+
+            if level >= 2:  # the interpolation is needed for both coarse and fine on reference xnodesc
+
+                QoI_coarse_interp[i,j,:] = interp1d(QoI_coarse[i][-1], QoI_coarse[i][j], kind='linear', fill_value='extrapolate')(xnodesc_ref)
+                QoI_fine_interp[i,j,:]   = interp1d(QoI_fine[i][-1],   QoI_fine[i][j],   kind='linear', fill_value='extrapolate')(xnodesc_ref)
+
+            elif level == 1: # the interpolation is needed for fine on reference xnodesc
+
+                QoI_fine_interp[i,j,:] = interp1d(QoI_fine[i][-1], QoI_fine[i][j], kind='linear', fill_value='extrapolate')(xnodesc_ref)
+
+            elif level == 0:
+
+                QoI_fine_interp[i,j,:] = np.array(QoI_fine[i][j])
+
+            # moving average
+            QoI_coarse_interp[i,j,:] = moving_average(QoI_coarse_interp[i,j,:], ws)
+            QoI_fine_interp[i,j,:]   = moving_average(QoI_fine_interp[i,j,:],   ws)
+
         
-        # RESULTS UPDATE
+    # RESULTS UPDATE
 
-        # P
-        sums1 += Pf - Pc 
-        sums2 += (Pf - Pc) ** 2
-        sums5 += Pf
-        sums6 += Pf ** 2
+    # [beta_n, beta_o, beta_no, beta_n2, beta_o2, P, Ttr, Tve, M, xnodes]
 
-        # N
-        sums1N += beta_Nf - beta_Nc
-        sums2N += (beta_Nf - beta_Nc) ** 2
+    # P
+    sums1 = np.sum(QoI_fine_interp[:,5,:]    - QoI_coarse_interp[:,5,:], axis=0)
+    sums2 = np.sum(QoI_fine_interp[:,5,:]**2 - QoI_coarse_interp[:,5,:]**2, axis=0)
+    sums5 = np.sum(QoI_fine_interp[:,5,:], axis=0)
+    sums6 = np.sum(QoI_fine_interp[:,5,:]**2, axis=0)
 
-        # O
-        sums1O += beta_Of - beta_Oc
-        sums2O += (beta_Of - beta_Oc) ** 2
+    # N
+    sums1N = np.sum(QoI_fine_interp[:,0,:]    - QoI_coarse_interp[:,0,:], axis=0)
+    sums2N = np.sum(QoI_fine_interp[:,0,:]**2 - QoI_coarse_interp[:,0,:]**2, axis=0)
 
-        # NO
-        sums1NO += beta_NOf - beta_NOc
-        sums2NO += (beta_NOf - beta_NOc) ** 2
+    # O
+    sums1O = np.sum(QoI_fine_interp[:,1,:]    - QoI_coarse_interp[:,1,:], axis=0)
+    sums2O = np.sum(QoI_fine_interp[:,1,:]**2 - QoI_coarse_interp[:,1,:]**2, axis=0)
 
-        # N2
-        sums1N2 += beta_N2f - beta_N2c
-        sums2N2 += (beta_N2f - beta_N2c) ** 2
+    # NO
+    sums1NO = np.sum(QoI_fine_interp[:,2,:]    - QoI_coarse_interp[:,2,:], axis=0)
+    sums2NO = np.sum(QoI_fine_interp[:,2,:]**2 - QoI_coarse_interp[:,2,:]**2, axis=0)
 
-        # O2
-        sums1O2 += beta_O2f - beta_O2c
-        sums2O2 += (beta_O2f - beta_O2c) ** 2
+    # N2
+    sums1N2 = np.sum(QoI_fine_interp[:,3,:]    - QoI_coarse_interp[:,3,:], axis=0)
+    sums2N2 = np.sum(QoI_fine_interp[:,3,:]**2 - QoI_coarse_interp[:,3,:]**2, axis=0)
+
+    # O2
+    sums1O2 = np.sum(QoI_fine_interp[:,4,:]    - QoI_coarse_interp[:,4,:], axis=0)
+    sums2O2 = np.sum(QoI_fine_interp[:,4,:]**2 - QoI_coarse_interp[:,4,:]**2, axis=0)
+    
+    # Temperatures
+    sums1Ttr = np.sum(QoI_fine_interp[:,6,:]    - QoI_coarse_interp[:,6,:], axis=0)
+    sums2Ttr = np.sum(QoI_fine_interp[:,6,:]**2 - QoI_coarse_interp[:,6,:]**2, axis=0)
+    sums1Tve = np.sum(QoI_fine_interp[:,7,:]    - QoI_coarse_interp[:,7,:], axis=0)
+    sums2Tve = np.sum(QoI_fine_interp[:,7,:]**2 - QoI_coarse_interp[:,7,:]**2, axis=0)
+
+    # M
+    sums1M = np.sum(QoI_fine_interp[:,8,:]    - QoI_coarse_interp[:,8,:], axis=0)
+    sums2M = np.sum(QoI_fine_interp[:,8,:]**2 - QoI_coarse_interp[:,8,:]**2, axis=0)
+    
+    # End of recording time
+    end   = time.time() 
+    cost  = nproc*(end-start)    # Cost computation
         
-        # Temperatures
-        sums1Ttr += Ttr_if - Ttr_ic
-        sums2Ttr += (Ttr_if - Ttr_ic) ** 2
-        sums1Tve += Tve_if - Tve_ic
-        sums2Tve += (Tve_if - Tve_ic) ** 2
-
-        # M
-        sums1M += M_if - M_ic
-        sums2M += (M_if - M_ic) ** 2
-        
-        # End of recording time
-        end   = time.time() 
-        cost  = nproc*(end-start)    # Cost computation
-        
-    return xnodesc, sums1, sums2, sums5, sums6, sums1N, sums2N, sums1O, sums2O, sums1NO, sums2NO, sums1N2, sums2N2, sums1O2, sums2O2, sums1Ttr, sums2Ttr, sums1Tve, sums2Tve, sums1M, sums2M, cost
+    return xnodesc_ref, sums1, sums2, sums5, sums6, sums1N, sums2N, sums1O, sums2O, sums1NO, sums2NO, sums1N2, sums2N2, sums1O2, sums2O2, sums1Ttr, sums2Ttr, sums1Tve, sums2Tve, sums1M, sums2M, cost
 
 
 def moving_average(data, window_size):
